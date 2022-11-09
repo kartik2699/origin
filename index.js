65ffc839c646b8275152813ae5faabb5fb49e6a2
Kartik <k.gatla2@zensar.com>
2022-11-09 15:08:14 +0530

    Index.js file with add(), sub()

---
+++ index.js
@@ -0,0 +1,10 @@
+function add(n1,n2){
+    console.log("sum is : "+n1+n2);
+}
+
+function sub(n1,n2){
+    console.log("sum is : "+n1-n2);
+}
+
+console.prompt(add(10,20));
+console.prompt(sub(100,20));
